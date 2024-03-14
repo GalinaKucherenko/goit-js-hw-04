e5b27d8ba0c2744e955f6b8092a5c32d1c31184e
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-03-14 12:10:57 +0200

    Home-Work

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,18 @@
 function calcAverageCalories(days) {
+
   let totalCalories = 0;
-   
-  for (const day of days) {
-    totalCalories += day.calories;
+
+    if (days.length === 0) {
+    return 0;
+  }
+  
+    for (const day of days) {
+      totalCalories += day.calories;
   } 
+  
     return totalCalories / days.length;
-}
+    
+  }
 
 
 console.log(
